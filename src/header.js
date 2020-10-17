5aa25d313df768327d864491eb839ff0ab5dae44
Balram Rathore <balram0698@gmail.com>
2020-10-17 21:45:23 +0530

    ne wnavbar added

--- src/header.js
+++ src/header.js
@@ -1,43 +1,116 @@
-import React,{useState} from "react"
-function Nav(){
-  const col="bg-teal-500"
-const[bg,setBg]=useState(col)
-const[count,setCount]=useState("1")
-const dark=()=>{
-  if(count==="1"){
-  setBg("bg-gray-900")
-   setCount("2")  
-}
-else{
-  setBg("bg-teal-500")
-  setCount("1")
-}
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import { Box } from "@material-ui/core";
 
-    document.querySelectorAll('.inverted').forEach((result)=>{
-      result.classList.toggle('invert')
-    })
-}
-    return(
-        <header class={`text-gray-500 ${bg} body-font`}>
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-    
-    </nav>
-    <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-teal-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl xl:block lg:hidden">Memes Maker</span>
-    </a>
-    <label className="switch" style={{backgroundColor: bg}}>
-             <input type="checkbox" onClick={dark}/>
-              <span className="slider round"></span>
-             </label>
-    <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-     
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+
+  title: {
+    flexGrow: 1,
+    display: "none",
+    fontSize: 24,
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.black, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+  formControl: {
+    // color: "black",
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+  const [dark, setDark] = useState(false);
+  const [bg, setbg] = useState("teal");
+  const handleChange = (event) => {
+    setDark(event.target.checked);
+    if (event.target.checked === true) {
+      setbg("black");
+    } else {
+      setbg("teal");
+    }
+
+    document.documentElement.classList.toggle("dark-mode");
+    document.querySelectorAll(".inverted").forEach((result) => {
+      result.classList.toggle("invert");
+    });
+  };
+ 
+  return (
+    <div className={classes.root}>
+      <AppBar style={{ backgroundColor: bg }} position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+          
+              Meme Maker
+       
+          </Typography>
+          <div className={classes.search}>
+            
+                       </div>
+          <Box p={1.5} />
+          <FormGroup>
+            <FormControlLabel
+              className={classes.formControl}
+              control={
+                <Switch
+                  checked={dark}
+                  onChange={handleChange}
+                  aria-label="Dark switch"
+                />
+              }
+              label={"Dark Mode"}
+            />
+          </FormGroup>
+        </Toolbar>
+      </AppBar>
     </div>
-  </div>
-</header>
-    )   
+  );
 }
-export default Nav
